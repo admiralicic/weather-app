1e812e23bb62dcd3638cb431730a404478bb5435
Admir Alicic <admir_alicic@yahoo.com>
2018-07-30 07:42:14 +0200

    Wrap request inside promise

--- playground/promise-2.js
+++ playground/promise-2.js
@@ -1,3 +1,32 @@
-var geocodeAddress = (address) => {
+const request = require('request');
 
+var geocodeAddress = (address) => {
+    return new Promise((resolve, reject) => {
+        let encodedAddress = encodeURIComponent(address);
+    
+        request({
+            url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyB3381FOAmcPqgShdY0Y2jHqnVAOmOYSqQ`,
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                reject('Unable to connect to Google servers.');
+            } else if (body.status === 'ZERO_RESULTS') {
+                reject('Unable to find the address.');
+            } else if (body.status === 'OK') {
+                let result = body.results[0];
+
+                resolve({
+                    address: result.formatted_address,
+                    latitude: result.geometry.location.lat,
+                    longitude: result.geometry.location.lng
+                });
+            }
+        });
+    });
 };
+
+geocodeAddress('Admira Dedica 60 75000 tuzla').then((location) => {
+    console.log(JSON.stringify(location));
+}).catch((errorMessage) => {
+    console.log(errorMessage);
+});
